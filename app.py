b3ac558f906bf41b08648c67af46d558218e5674
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2024-10-22 21:27:27 +0100

    issues fixed
    half way through adding flask multiple pages

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/Home')
 def home():
-    return render_template("home.html")
+    return render_template("Home.html")
+
+@app.route('/About')
+def about():
+    return render_template("About.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
