73b323b27ddeebc914d8de5751027ba54552a988
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2024-10-23 16:42:22 +0100

    Page links and CSS added
    added to app.py for the other webpages , also added css file linking to all other html files.

--- app.py
+++ app.py
@@ -2,13 +2,37 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#Landing page
+@app.route('/')
+def landing():
+    print('test landing page')
+    return render_template('Home.html')
+
+#Home page return
 @app.route('/Home')
 def home():
+    print('test home return')
     return render_template("Home.html")
 
+#Infomation page
+@app.route('/Infomation')
+def infomation():
+    print('test infomation')
+    return render_template("Infomation.html")
+
+#About page
 @app.route('/About')
 def about():
-    return render_template("About.html")
+    print('test about page')
+    return render_template('About.html')
+
+#Login page
+@app.route('/Login')
+def Login():
+    print('test Login page')
+    return render_template("Login.html")
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
