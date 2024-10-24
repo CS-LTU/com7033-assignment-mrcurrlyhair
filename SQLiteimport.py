13e31b1c86a0b33d811ef84befc8c824dfda4a52
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2024-10-24 21:33:12 +0100

    testing creating database

--- SQLiteimport.py
+++ SQLiteimport.py
@@ -12,19 +12,20 @@ cur = con.cursor()
 #Creating the table for patient infoimation 
 cur.execute('''
     CREATE TABLE IF NOT EXISTS patient(
-        id INTEGER PRIMART KEY, 
-        gender TEXT(6), 
-        age INTEGER, 
-        hypertension , 
-        heart_disease, 
-        ever_married, 
-        work_type, 
-        residence_type, 
-        avg_glucose_level, 
-        bmi, 
-        smoking_status, 
-        stroke
+        p_id INTEGER PRIMARY KEY, 
+        p_gender TEXT(6) NOT NULL, 
+        p_age INTEGER NOT NULL, 
+        p_hypertension BOOLEAN NOT NULL, 
+        p_heart_disease BOOLEAN NOT NULL, 
+        p_ever_married BOOLEAN NOT NULL, 
+        p_work_type TEXT NOT NULL, 
+        p_residence_type TEXT NOT NULL, 
+        p_avg_glucose_level FLOAT NOT NULL, 
+        p_bmi FLOAT, 
+        p_smoking_status BOOLEAN NOT NULL, 
+        p_stroke BOOLEAN NOT NULL
     )
 ''')
-        
+
+
 
