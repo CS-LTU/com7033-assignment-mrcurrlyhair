6c2c07c1537121b6943866d3e700ae120a8d4bee
mrcurrlyhair <mrcurrlyhair@users.noreply.github.com>
2024-10-24 20:52:52 +0100

    Adding data to table v0.5

--- SQLiteimport.py
+++ SQLiteimport.py
@@ -6,12 +6,25 @@ db_path = 'Database.db'
 csv_path = 'healthcare-dataset-stroke-data.csv'
 
 
-
-
-
-con = sqlite3.connect("Database.db")
-
+con = sqlite3.connect(db_path)
 cur = con.cursor()
 
-cur.execute("CREATE TABLE Patient(id, gender, age, hypertension, heart_disease, ever_married, work_type, residence_type, avg_glucose_level, bmi, smoking_status, stroke)")
+#Creating the table for patient infoimation 
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS patient(
+        id INTEGER PRIMART KEY, 
+        gender TEXT(6), 
+        age INTEGER, 
+        hypertension , 
+        heart_disease, 
+        ever_married, 
+        work_type, 
+        residence_type, 
+        avg_glucose_level, 
+        bmi, 
+        smoking_status, 
+        stroke
+    )
+''')
+        
 
